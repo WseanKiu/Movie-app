12a470e83002450d7eb02ad1a19e86e146125c6d
Winston Neo <winstonseanqui@gmail.com>
2025-04-03 21:25:49 +0800

    search functionality

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,5 +1,5 @@
 import { Text, View, Image, FlatList, ActivityIndicator } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { images } from '@/constants/images';
 import MovieCard from '@/components/MovieCard';
 import useFetch from '@/services/useFetch';
@@ -14,11 +14,25 @@ const search = () => {
   const {
     data: movies, 
     loading, 
-    error 
+    error,
+    refetch: loadMovies,
+    reset,
   } = useFetch(() => fetchMovies({
-    query: '',
+    query: searchQuery,
   }), false);
-  
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset();
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
   return (
     <View className='flex-1 bg-primary'>
       <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode='cover'/>
@@ -33,14 +47,18 @@ const search = () => {
           gap: 16,
           marginVertical: 16
         }}
-        contentContainerStyle={{ paddingBottom: 100 }}
+        contentContainerStyle={ { paddingBottom: 100 }}
         ListHeaderComponent={
           <>
             <View className='w-full flex-row justify-center mt-20 items-center'>
               <Image source={icons.logo} className='w-12 h-10' resizeMode='contain'/>
             </View>
             <View className='my-5'>
-              <SearchBar placeholder='Search movies...' />
+              <SearchBar 
+                placeholder='Search movies...'
+                value={searchQuery}
+                onChangeText={(text:string) => setSearchQuery(text)}
+                />
             </View>
             {loading && (
               <ActivityIndicator size='large' color={'#0000ff'} className='my-3' />
@@ -52,13 +70,22 @@ const search = () => {
               </Text>
             )}
 
-            {!loading && !error && 'SEARCH TERM'.trim() && movies?.length > 0 && (
+            {!loading && !error && searchQuery.trim() && movies?.length > 0 && (
               <Text className='text-xl text-white font-bold'>
                 Search Results for{' '}
-                <Text className='text-accent'>SEARCH TERM</Text>
+                <Text className='text-accent'>{searchQuery}</Text>
               </Text>
             )}
           </>
+        }
+        ListEmptyComponent={
+          !loading && !error ? (
+            <View className='mt-10 px-5'>
+              <Text className='text-gray-500 text-center'>
+                {searchQuery.trim() ? 'No results found' : 'Search for movies...'}
+              </Text>
+            </View>
+          ) : null
         }
         />
     </View>
